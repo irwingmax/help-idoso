243581460ca7a501aef67c91bb56cc2e8ca3b08e
irwingmax <mecatronica2020@outlook.com>
2018-04-12 16:04:06 -0300

    project v2

---
+++ src/pages/tipo-ajuda/tipo-ajuda.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TipoAjudaPage } from './tipo-ajuda';
+
+@NgModule({
+  declarations: [
+    TipoAjudaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TipoAjudaPage),
+  ],
+})
+export class TipoAjudaPageModule {}
